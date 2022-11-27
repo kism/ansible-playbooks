52a6542970526d860809fa68e554a9c89b933c1f
Kieran Gee <kieran.lost.the.game@gmail.com>
2022-11-27 19:51:49 +0800

    add DPSHUB public key

--- onboard/init.sh
+++ onboard/init.sh
@@ -2,7 +2,8 @@
 packages="curl tar file"
 dnf --setopt=install_weak_deps=False --best install -y $packages || apk add $packages || apt-get --no-install-recommends -y $packages || pacman -S --noconfirm $packages || pkg install $packages
 
-publickey="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCzVh8CktGh+5LlZJfeBj/VcRzGO6zjrW2p2dM+X4PA ansible_svc"
+publickeys="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCzVh8CktGh+5LlZJfeBj/VcRzGO6zjrW2p2dM+X4PA ansible_svc
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDLGih8ZZL9fwTCiKKmlyrHJXVQvKENZQMf2+/G8z65 root@DPSHUB"
 account="ansible_svc"
 group="kgadmins"
 
@@ -12,7 +13,7 @@ usermod -a -G $group $account
 chage -I -1 -m 0 -M 99999 -E -1 $account
 mkdir -p /home/$account/.ssh
 echo "%$group ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/$group
-echo $publickey > /home/$account/.ssh/authorized_keys
+echo $publickeys > /home/$account/.ssh/authorized_keys
 chown -R $account:$account /home/$account
 chmod 700 /home/$account/.ssh
 chmod 600 /home/$account/.ssh/authorized_keys
