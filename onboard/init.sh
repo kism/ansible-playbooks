ba1789e8c85c9d466fb2294df4415fd7444bc7ee
Kieran Gee <kieran.lost.the.game@gmail.com>
2022-11-27 19:56:02 +0800

    fix lines

--- onboard/init.sh
+++ onboard/init.sh
@@ -2,8 +2,7 @@
 packages="curl tar file"
 dnf --setopt=install_weak_deps=False --best install -y $packages || apk add $packages || apt-get --no-install-recommends -y $packages || pacman -S --noconfirm $packages || pkg install $packages
 
-publickeys="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCzVh8CktGh+5LlZJfeBj/VcRzGO6zjrW2p2dM+X4PA ansible_svc
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDLGih8ZZL9fwTCiKKmlyrHJXVQvKENZQMf2+/G8z65 root@DPSHUB"
+publickeys="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCzVh8CktGh+5LlZJfeBj/VcRzGO6zjrW2p2dM+X4PA ansible_svc\nssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDLGih8ZZL9fwTCiKKmlyrHJXVQvKENZQMf2+/G8z65 root@DPSHUB"
 account="ansible_svc"
 group="kgadmins"
 
@@ -13,7 +12,7 @@ usermod -a -G $group $account
 chage -I -1 -m 0 -M 99999 -E -1 $account
 mkdir -p /home/$account/.ssh
 echo "%$group ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/$group
-echo $publickeys > /home/$account/.ssh/authorized_keys
+echo -e $publickeys > /home/$account/.ssh/authorized_keys
 chown -R $account:$account /home/$account
 chmod 700 /home/$account/.ssh
 chmod 600 /home/$account/.ssh/authorized_keys
