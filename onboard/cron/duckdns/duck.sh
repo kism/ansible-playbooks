e586d193341f08777d0cec12e96b15422df666f9
Kieran Gee <kieran.lost.the.game@gmail.com>
2022-11-27 18:10:57 +0800

    updates

---
+++ onboard/cron/duckdns/duck.sh
@@ -0,0 +1,5 @@
+. /opt/duckdns/ducktoken
+ipv4addr=$(curl -s ipv4.icanhazip.com)
+ipv6addr=$(curl -s ipv6.icanhazip.com)
+url="https://www.duckdns.org/update?domains=kism&token=$ducktoken&ip=$ipv4addr&ipv6=$ipv6addr"
+curl -s -k -o /var/log/duckdns.log $url
