9c7b3f650dea5ef1d2fb57c1aa7b619b7f5f9031
Kieran Gee <kieran.lost.the.game@gmail.com>
2022-12-24 13:50:14 +0800

    duckdns for the factorio server

--- onboard/scripts_cron/duck.sh
+++ onboard/scripts_cron/duck.sh
@@ -2,5 +2,6 @@ sleep $(shuf -i 0-500 -n 1)
 ducktoken=$(cat /opt/duckdns/ducktoken)
 ipv4addr=$(curl -s ipv4.icanhazip.com)
 ipv6addr=$(curl -s ipv6.icanhazip.com)
-url="https://www.duckdns.org/update?domains=kism&token=$ducktoken&ip=$ipv4addr&ipv6=$ipv6addr"
+domain=kism
+url="https://www.duckdns.org/update?domains=$domain&token=$ducktoken&ip=$ipv4addr&ipv6=$ipv6addr"
 curl -s -k -o /var/log/duckdns.log $url
