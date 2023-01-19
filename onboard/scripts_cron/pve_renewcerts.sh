1dbc23da71b01ab98340dcf3a2b3b48e6206cd2b
Kieran Gee <kieran.lost.the.game@gmail.com>
2023-01-19 21:37:33 +0800

    add some things, fix some things

--- onboard/scripts_cron/pve_renewcerts.sh
+++
@@ -1,3 +0,0 @@
-cp /etc/letsencrypt/live/pve.kg.lan/fullchain.pem /etc/pve/local/pveproxy-ssl.pem
-cp /etc/letsencrypt/live/pve.kg.lan/privkey.pem /etc/pve/local/pveproxy-ssl.key
-systemctl reload pveproxy
