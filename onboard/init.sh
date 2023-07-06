38ca30861fae0ca7e36d5710321cde5c9fe8eb5f
Kieran Gee <kieran.lost.the.game@gmail.com>
2023-07-06 21:12:44 +0800

    😎

--- onboard/init.sh
+++ onboard/init.sh
@@ -1,5 +1,5 @@
 # curl --silent https://raw.githubusercontent.com/kism/ansible-playbooks/master/onboard/init.sh | bash
-packages="curl tar sudo acl"
+packages="curl tar sudo acl python3"
 dnf --setopt=install_weak_deps=False --best install -y $packages || apk add $packages || apt-get --no-install-recommends -y install $packages || pacman -S --noconfirm $packages || pkg install $packages
 
 publickeys="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCzVh8CktGh+5LlZJfeBj/VcRzGO6zjrW2p2dM+X4PA ansible_svc\nssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDLGih8ZZL9fwTCiKKmlyrHJXVQvKENZQMf2+/G8z65 root@DPSHUB"
