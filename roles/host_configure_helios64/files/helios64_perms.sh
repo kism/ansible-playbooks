164db2101c43dc99bfa2756f8f32b531c766df6e
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-03-25 10:12:24 +0800

    nfs defined proper

--- roles/host_configure_helios64/files/helios64_perms.sh
+++ roles/host_configure_helios64/files/helios64_perms.sh
@@ -1,44 +1,44 @@
-# /mnt
+# /srv
 
 ## Remove ACLs
-setfacl --remove-all --recursive /mnt/Quina
-setfacl --remove-all --recursive /mnt/Quale
-setfacl --remove-all --recursive /mnt/Steiner
+setfacl --remove-all --recursive /srv/Quina
+setfacl --remove-all --recursive /srv/Quale
+setfacl --remove-all --recursive /srv/Steiner
 
 ## Let everyone get to the hard drive partitions
-chown root:root /mnt
-chmod u=rwX,g=rX,o=rX /mnt
-chmod g-s /mnt
-chmod g-s /mnt/*
-chmod u=rwX,g=rX,o=rX /mnt/Quina
-chmod u=rwX,g=rX,o=rX /mnt/Quale
-chmod u=rwX,g=rX,o=rX /mnt/Steiner
+chown root:root /srv
+chmod u=rwX,g=rX,o=rX /srv
+chmod g-s /srv
+chmod g-s /srv/*
+chmod u=rwX,g=rX,o=rX /srv/Quina
+chmod u=rwX,g=rX,o=rX /srv/Quale
+chmod u=rwX,g=rX,o=rX /srv/Steiner
 
 ## Set Owners
-chown kism:content_public /mnt/Quina
-chown kism:content_public /mnt/Quale
-chown kism:content_public /mnt/Steiner
+chown kism:content_public /srv/Quina
+chown kism:content_public /srv/Quale
+chown kism:content_public /srv/Steiner
 
 # Subfolders
 
 ## Set Permissions for Content
-chown -R kism:content_private /mnt/Quina/z
-chown -R qbtuser:qbtuser /mnt/Quina/downloads
-chown -R kism:content_private /mnt/Quina/configs
+chown -R kism:content_private /srv/Quina/z
+chown -R qbtuser:qbtuser /srv/Quina/downloads
+chown -R kism:content_private /srv/Quina/configs
 
 ## Set setgid to directories recursively
-find /mnt/Quina   -mindepth 1 -type d -exec chmod g+s {} +
-find /mnt/Quale   -mindepth 1 -type d -exec chmod g+s {} +
-find /mnt/Steiner -mindepth 1 -type d -exec chmod g+s {} +
+find /srv/Quina   -mindepth 1 -type d -exec chmod g+s {} +
+find /srv/Quale   -mindepth 1 -type d -exec chmod g+s {} +
+find /srv/Steiner -mindepth 1 -type d -exec chmod g+s {} +
 
 ## Recursively set perms on subfolders of all content mounts
-find /mnt/Quina   -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rX,o-wrx {} +
-find /mnt/Quale   -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rX,o-wrx {} +
-find /mnt/Steiner -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rX,o-wrx {} +
+find /srv/Quina   -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rX,o-wrx {} +
+find /srv/Quale   -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rX,o-wrx {} +
+find /srv/Steiner -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rX,o-wrx {} +
 
 # Misc
 
 ## Fun file exceptions
-chown root:root /mnt/*/lost+found
-chmod 0700 /mnt/*/lost+found
-chmod g-s /mnt/*/lost+found # huh
+chown root:root /srv/*/lost+found
+chmod 0700 /srv/*/lost+found
+chmod g-s /srv/*/lost+found # huh
