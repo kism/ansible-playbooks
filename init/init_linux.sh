f5674a98238cd39e451e1674b2bdb4449923cdd9
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-04-23 13:57:46 +0800

    user onboarding

--- init/init_linux.sh
+++ init/init_linux.sh
@@ -1,26 +1,20 @@
+#!/usr/bin/env bash
+
+# Setup a VM for ansible
 # curl --silent https://raw.githubusercontent.com/kism/ansible-playbooks/master/init/init_linux.sh | bash
+
+# All users and groups defined in https://github.com/kism/ansible-playbooks/blob/main/roles/configure_users/vars/main.yml
+# This script onboards the a user manually for onboarding
+account="ansible_svc"
+account_uid=1088
+group="kgadmins"
+group_gid=1099
+
 packages="curl tar sudo acl python3 openssh-server"
 dnf --setopt=install_weak_deps=False --best install -y $packages || apk add $packages || apt-get --no-install-recommends -y install $packages || pacman -S --noconfirm $packages || pkg install $packages
 
-account="ansible_svc"
-group="kgadmins"
-
-# GROUPS
-# 1099 kgadmins
-# 1098 content_private
-# 1097 content_public
-
-# USERS, USER GID IS THE SAME
-# 1089 kism
-# 1088 ansible_svc
-# 1087 backup_svc
-# 1086 qbtuser
-# 1085 lanprivate
-# 1084 lanuser
-# 1083 podcasto
-
-id -u $account &>/dev/null || useradd --uid 1088 $account
-groupadd --gid 1099 $group
+id -u $account &>/dev/null || useradd --uid $account_uid $account
+groupadd --gid $group_gid $group
 usermod -a -G $group $account
 usermod -p '*' $account
 chage -I -1 -m 0 -M 99999 -E -1 $account
