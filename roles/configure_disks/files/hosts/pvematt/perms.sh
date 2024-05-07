07af81c93795dcd26577258b0144a6927bb2201f
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-05-07 16:06:46 +0800

    fix rad

--- roles/configure_disks/files/hosts/pvematt/perms.sh
+++ roles/configure_disks/files/hosts/pvematt/perms.sh
@@ -1,6 +1,8 @@
-# /srv
+#! /usr/bin/env bash
 
-## Remove ACLs
+echo -- /srv
+
+echo - Remove ACLs
 setfacl --remove-all --recursive /srv/Amarant
 setfacl --remove-all --recursive /srv/Freya
 setfacl --remove-all --recursive /srv/Garnet
@@ -8,7 +10,8 @@ setfacl --remove-all --recursive /srv/Vivi
 setfacl --remove-all --recursive /srv/Eiko
 setfacl --remove-all --recursive /srv/Zidane
 
-## Let everyone get to the hard drive partitions, no group write to prevent files getting created directly in them
+echo - Let everyone get to the hard drive partitions,
+echo   no group write to prevent files getting created directly in them
 chown root:root /srv
 chmod u=rwX,g=rX,o=rX /srv
 chmod g-s /srv
@@ -20,7 +23,7 @@ chmod u=rwX,g=rX,o=rX /srv/Vivi
 chmod u=rwX,g=,o= /srv/Eiko # only touched by snapraid
 chmod u=rwX,g=,o= /srv/Zidane # only touched by snapraid
 
-## Set Owners
+echo - Set Owners
 chown root:root /srv/Amarant
 chown root:root /srv/Freya
 chown root:root /srv/Garnet
@@ -28,12 +31,13 @@ chown root:root /srv/Vivi
 chown root:root /srv/Eiko # only touched by snapraid
 chown root:root /srv/Zidane # only touched by snapraid
 
-# Subfolders
+echo -- Subfolders
 
-## Set Permissions for Content
+echo - Set Permissions for Content
 chown -R backup_svc:backup_svc /srv/Amarant/backups
 chown -R kism:content_private  /srv/Amarant/Downloads
 chown -R kism:content_private  /srv/Amarant/Temp
+chown -R kism:content_public   /srv/Freya/Video
 
 chown -R kism:content_public  /srv/Freya/Apps
 chown -R kism:content_public  /srv/Freya/Emulators
@@ -53,27 +57,27 @@ chown -R kism:content_private /srv/Vivi/Pictures
 chown -R kism:content_public  /srv/Vivi/ps2smb
 chown -R kism:content_public  /srv/Vivi/Video
 
-## Find all dirs, set setguid
+echo -- Finds
+
+echo - Find all dirs, set setguid
 find /srv/Amarant -mindepth 1 -type d -exec chmod g+s {} +
 find /srv/Freya   -mindepth 1 -type d -exec chmod g+s {} +
 find /srv/Garnet  -mindepth 1 -type d -exec chmod g+s {} +
 find /srv/Vivi    -mindepth 1 -type d -exec chmod g+s {} +
 
-## Find all files, remove all special bits
+echo - Find all files, remove all special bits
 find /srv/Amarant -type f -exec chmod u-s,g-s,o-s {} +
 find /srv/Freya   -type f -exec chmod u-s,g-s,o-s {} +
 find /srv/Garnet  -type f -exec chmod u-s,g-s,o-s {} +
 find /srv/Vivi    -type f -exec chmod u-s,g-s,o-s {} +
 
-## Recursively set perms on subfolders of all content mounts
+echo - Recursively set perms on subfolders of all content mounts
 find /srv/Amarant -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rwX,o= {} +
 find /srv/Freya   -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rwX,o= {} +
 find /srv/Garnet  -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rwX,o= {} +
 find /srv/Vivi    -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rwX,o= {} +
 
-# Misc
-
-## Fun file exceptions
+echo -- Fun file exceptions
 chown root:root /srv/*/lost+found
 chmod 0700 /srv/*/lost+found
 chmod g-s /srv/*/lost+found # huh
--- roles/configure_disks/files/hosts/pvewill/perms.sh
+++ roles/configure_disks/files/hosts/pvewill/perms.sh
@@ -1,11 +1,14 @@
-# /srv
+#! /usr/bin/env bash
 
-## Remove ACLs
+echo -- /srv
+
+echo - Remove ACLs
 setfacl --remove-all --recursive /srv/Quina
 setfacl --remove-all --recursive /srv/Quale
 setfacl --remove-all --recursive /srv/Steiner
 
-## Let everyone get to the hard drive partitions, no group write to prevent files getting created directly in them
+echo - Let everyone get to the hard drive partitions,
+echo   no group write to prevent files getting created directly in them
 chown root:root /srv
 chmod u=rwX,g=rX,o=rX /srv
 chmod g-s /srv
@@ -14,36 +17,35 @@ chmod u=rwX,g=rX,o=rX /srv/Quina
 chmod u=rwX,g=rX,o=rX /srv/Quale
 chmod u=rwX,g=rX,o=rX /srv/Steiner
 
-## Set Owners
+echo - Set Owners
 chown root:root /srv/Quina
 chown root:root /srv/Quale
 chown root:root /srv/Steiner
 
-# Subfolders
+echo -- Subfolders
 
-## Set Permissions for Content
+echo - Set Permissions for Content
 chown -R kism:content_private /srv/Quina/z
 chown -R qbtuser:qbtuser /srv/Quina/downloads
 chown -R qbtuser:qbtuser /srv/Quina/configs
 
-## Find all dirs, set setguid
-find /srv/Quina    -mindepth 1 -type d -exec chmod g+s {} +
+echo -- Finds
+
+echo - Find all dirs, set setguidfind /srv/Quina    -mindepth 1 -type d -exec chmod g+s {} +
 find /srv/Quale    -mindepth 1 -type d -exec chmod g+s {} +
 find /srv/Steiner  -mindepth 1 -type d -exec chmod g+s {} +
 
-## Find all files, remove all special bits
+echo - Find all files, remove all special bits
 find /srv/Quina   -type f -exec chmod u-s,g-s,o-s {} +
 find /srv/Quale   -type f -exec chmod u-s,g-s,o-s {} +
 find /srv/Steiner -type f -exec chmod u-s,g-s,o-s {} +
 
-## Recursively set perms on subfolders of all content mounts
+echo - Recursively set perms on subfolders of all content mounts
 find /srv/Quina   -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rwX,o= {} +
 find /srv/Quale   -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rwX,o= {} +
 find /srv/Steiner -mindepth 1 -maxdepth 1 -type d -exec chmod -R u=rwX,g+rwX,o= {} +
 
-# Misc
-
-## Fun file exceptions
+echo -- Fun file exceptions
 chown root:root /srv/*/lost+found
 chmod 0700 /srv/*/lost+found
 chmod g-s /srv/*/lost+found # huh
