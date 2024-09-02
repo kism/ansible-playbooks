b1a8acb2091980344be520f19a598cdbf5dafa2c
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-09-02 21:30:22 +0800

    elastic

--- roles/configure_elk/files/certbot_elastic.sh
+++ roles/configure_elk/files/certbot_elastic.sh
@@ -6,7 +6,7 @@ python3 -c 'import random; import time; time.sleep(random.random() * 3600)'
 
 /usr/bin/certbot renew -q
 
-cp /etc/letsencrypt/live/kierangee.au/* /etc/elasticsearch/certs/
+cp /etc/letsencrypt/live/elastic.kierangee.au/* /etc/elasticsearch/certs/
 
 chown -R root:elasticsearch /etc/elasticsearch/certs/*
 
