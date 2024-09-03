d86e05a5c5906f8dd00a2bf23f9d3ec15d269ffe
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-09-03 13:17:44 +0800

    timers created for certbot

--- roles/configure_elk/files/elastalert2_run.sh
+++ roles/configure_elk/files/elastalert2_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 ~/.local/bin/elastalert-create-index --config /opt/elastalert2/config.yml
-~/.local/bin/elastalert --verbose --config /opt/elastalert2/config.yml
+~/.local/bin/elastalert --config /opt/elastalert2/config.yml
