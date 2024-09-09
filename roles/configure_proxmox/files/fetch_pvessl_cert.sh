c59e1067b66f3872a080be4e3a4a9673ed7b14ad
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-09-09 10:12:59 +0800

    better ssl in proxmox

---
+++ roles/configure_proxmox/files/fetch_pvessl_cert.sh
@@ -0,0 +1,2 @@
+#!/env/bin/bash
+openssl s_client -showcerts -connect localhost:8006 </dev/null 2>/dev/null | openssl x509 -outform PEM > /etc/ssl/certs/pveproxy-fetched.cer
--- roles/configure_proxmox/files/proxmox_renewcerts.sh
+++
@@ -1,3 +0,0 @@
-cp /etc/letsencrypt/live/HOSTNAME/fullchain.pem /etc/pve/local/pveproxy-ssl.pem
-cp /etc/letsencrypt/live/HOSTNAME/privkey.pem /etc/pve/local/pveproxy-ssl.key
-systemctl reload pveproxy
